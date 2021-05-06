624e6b3c260b4b461f5f36ba67c09465715ab228
LB Vik <laura.bullek@gmail.com>
2021-05-06 19:49:07 -0400

    Deleted Jest from testing

--- 12-Stu_TDD/Unsolved/test/algo.test.js
+++ 12-Stu_TDD/Unsolved/test/algo.test.js
@@ -1,9 +1,22 @@
+cd
 const Algo = require("../algo");
 
 describe("Algo", () => {
   describe("reverse", () => {
     // TODO: Write a test for the `reverse` method that should take a string as an argument and return a new reversed version of the string
-  });
+      // In order to test the functiuon, we need to creat3e a working example. First define "str"
+      const str = "Hello World";
+
+      // Next we define 'reversed' as the calue of 'str; reversed
+      const reversed = '!dlroW olleH';
+
+      // We pass 'str' into .reverse() and set that value to equal the result
+      const result = new Algo().reverse(str);
+
+      // We expect 'result' to equal 'reversed' if the 'reverse()' function is working properly
+      expect(result).toEqual(reversed);
+   });
+
 
   describe("isPalindrome", () => {
     // TODO: Write a test for the `isPalindrome` method that should take a string as an argument and return the boolean `true` if the provided string is a palindrome.
